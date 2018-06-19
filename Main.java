c47b6e948b3b990b70555d17060d5adcb6e8d04b
galustian <leonard@galustian.com>
2018-06-19 09:09:09 +0200

    refactor

--- Main.java
+++ Main.java
@@ -5,8 +5,8 @@ import java.awt.image.BufferedImage;
 public class Main {
     private static final int WIDTH = 1000;
     private static final int HEIGHT = 750;
-    private static final int ITERATIONS = 200;
-    public static final float SCALE = 200;
+    private static final int ITERATIONS = 50;
+    private static final float SCALE = 200;
 
 
     public static void main(String[] args) {
@@ -14,9 +14,9 @@ public class Main {
 
         var img = constructMandelbrotImage();
 
-        var panel = new JPanel() {
+        var panel = new JComponent() {
             @Override
-            protected void paintComponent(Graphics g) {
+            public void paint(Graphics g) {
                 super.paintComponent(g);
                 g.drawImage(img, 0, 0, null);
             }
@@ -24,8 +24,8 @@ public class Main {
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        frame.pack();
         frame.add(panel);
+        frame.pack();
         frame.setVisible(true);
     }
 
@@ -37,36 +37,54 @@ public class Main {
 
         // c = a + bi
         // (a + bi)(a + bi) = a^2 - b^2 + 2abi
-
         for (int i = 0; i < WIDTH; i++) {
             for (int j = 0; j < HEIGHT; j++) {
                 float x = (i - WIDTH / 2f) / SCALE;
                 float y = (j - HEIGHT / 2f) / SCALE;
 
-                Color color = getPointColor(x, y);
-                img.setRGB(i, j, color.getRGB());
+                int color = getPointColor(x, y);
+                img.setRGB(i, j, color);
             }
         }
 
         return img;
     }
 
-    private static Color getPointColor(float x, float y) {
+    /*private static int getPointColor(float x, float y) {
         float a_loop = x;
         float b_loop = y;
 
         float ac = a_loop;
         float bc = b_loop;
 
-        int k = 0;
-        for (; k < ITERATIONS; k++) {
+        int i = 0;
+        for (; i < ITERATIONS; i++) {
             a_loop = a_loop * a_loop - b_loop * b_loop + ac;
             b_loop = 2*a_loop*b_loop + bc;
 
             if (a_loop*a_loop + b_loop*b_loop > 5) break;
         }
 
-        if (k == ITERATIONS) return new Color(0, 0, 0);
-        return new Color(255, 255, 255);
+        if (i == ITERATIONS) return 0x000000;
+        return Color.HSBtoRGB((float)i / ITERATIONS, 0.5f, 1);
+    }*/
+
+    private static int getPointColor(float x, float y) {
+        float cx = x;
+        float cy = y;
+
+        int i = 0;
+
+        for(; i < ITERATIONS; i++) {
+            float nx = x*x - y*y + cx;
+            float ny = 2*x*y + cy;
+            x = nx;
+            y = ny;
+
+            if (x*x + y*y > 4) break;
+        }
+
+        if (i == ITERATIONS) return 0x000000;
+        return Color.HSBtoRGB((float)i / ITERATIONS, 0.5f, 1);
     }
 }
